399787a21f384e760efa520e3c89fb09df4eee4c
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-06-22 20:08:15 -0700

    render static index.ejs with express

---
+++ my-retail/server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = require('./routes/routes');
+const path = require('path');
+
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../client'));
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.use('/', router);
+
+module.exports = app;
