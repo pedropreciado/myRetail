ce98a0735c00e3cecd472887cd583b95f9830e4c
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-06-24 02:18:35 -0700

    add modal to render larger image

--- my-retail/client/components/product/show/product_images.js
+++ my-retail/client/components/product/show/product_images.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import ProductImageSlide from './product_image_slide';
 import { FaSearchPlus } from 'react-icons/lib/fa';
+import Modal from 'react-modal';
+
+import ProductImageSlide from './product_image_slide';
 
 export default class ProductImages extends React.Component {
   constructor(props) {
@@ -9,30 +11,44 @@ export default class ProductImages extends React.Component {
     
     this.state = {
       currentImageIdx: primaryImageIdx,
+      modalIsOpen: false,
     };
 
     this.changeImage = this.changeImage.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
   }
 
+  toggleModal() {
+    let modalIsOpen = !this.state.modalIsOpen;
+    this.setState({ modalIsOpen });
+  }
+  
   changeImage(currentImageIdx) {
     this.setState({ currentImageIdx });
   }
   
   render() {
-    let { images } = this.props;
-    let { currentImageIdx } = this.state;
+    let { images, itemTitle } = this.props;
+    let { currentImageIdx, modalIsOpen } = this.state;
     
     return (
       <div className='product-image-container'>
         <img id='primary-image' src={images[currentImageIdx]}/>
         
-        <a id='zoom-image'><FaSearchPlus />view larger</a>
+        <a onClick={this.toggleModal} id='zoom-image'><FaSearchPlus />view larger</a>
         
         <ProductImageSlide 
         images={images}
         currentImageIdx={currentImageIdx}
         changeImage={this.changeImage}
         />          
+        <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={this.toggleModal}
+        contentLabel={itemTitle}
+        >
+          <img id='in-modal' src={images[currentImageIdx]} />
+        </Modal>
       </div>
     );
   }
