dedbd2f58fb6ae9ac4ebae55e165d7a68b6c5e28
Pedro Preciado III <pedropreciadoiii@gmail.com>
2018-06-28 00:53:36 -0700

    set process env port

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const path = require('path');
 
 const app = express();
 
-
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
